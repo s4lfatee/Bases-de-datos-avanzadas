f106e1b8992f605cf9507f4d413ceaf28c11f94e
Grillitoxc <91287199+Grillitoxc@users.noreply.github.com>
2022-09-05 01:26:52 -0300

    Añadidos los archivos .sql

---
+++ dbCreate.sql
@@ -0,0 +1,404 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.5
+-- Dumped by pg_dump version 14.5
+
+-- Started on 2022-09-04 22:23:17
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 3367 (class 1262 OID 18042)
+-- Name: Galeria_de_arte; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE "Galeria_de_arte" WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'Spanish_Chile.1252';
+
+
+ALTER DATABASE "Galeria_de_arte" OWNER TO postgres;
+
+\connect "Galeria_de_arte"
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 209 (class 1259 OID 18043)
+-- Name: cliente; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.cliente (
+    id_cliente integer NOT NULL,
+    id_comuna integer NOT NULL,
+    cliente character varying(30) NOT NULL
+);
+
+
+ALTER TABLE public.cliente OWNER TO postgres;
+
+--
+-- TOC entry 210 (class 1259 OID 18046)
+-- Name: comuna; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.comuna (
+    id_comuna integer NOT NULL,
+    id_region integer NOT NULL,
+    comuna character varying(30) NOT NULL
+);
+
+
+ALTER TABLE public.comuna OWNER TO postgres;
+
+--
+-- TOC entry 211 (class 1259 OID 18049)
+-- Name: galeria; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.galeria (
+    id_galeria integer NOT NULL,
+    id_comuna integer NOT NULL,
+    galeria character varying(30) NOT NULL
+);
+
+
+ALTER TABLE public.galeria OWNER TO postgres;
+
+--
+-- TOC entry 212 (class 1259 OID 18052)
+-- Name: producto; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.producto (
+    id_producto integer NOT NULL,
+    id_tipo integer NOT NULL,
+    producto character varying(50) NOT NULL,
+    peso double precision NOT NULL,
+    id_galeria integer NOT NULL
+);
+
+
+ALTER TABLE public.producto OWNER TO postgres;
+
+--
+-- TOC entry 213 (class 1259 OID 18055)
+-- Name: region; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.region (
+    id_region integer NOT NULL,
+    region character varying(20) NOT NULL
+);
+
+
+ALTER TABLE public.region OWNER TO postgres;
+
+--
+-- TOC entry 214 (class 1259 OID 18058)
+-- Name: tipo; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.tipo (
+    id_tipo integer NOT NULL,
+    tipo character varying(10) NOT NULL
+);
+
+
+ALTER TABLE public.tipo OWNER TO postgres;
+
+--
+-- TOC entry 215 (class 1259 OID 18061)
+-- Name: vendedor; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.vendedor (
+    id_vendedor integer NOT NULL,
+    id_comuna integer NOT NULL,
+    id_galeria integer NOT NULL,
+    vendedor character varying(30) NOT NULL,
+    sexo character varying(2),
+    sueldo integer NOT NULL
+);
+
+
+ALTER TABLE public.vendedor OWNER TO postgres;
+
+--
+-- TOC entry 216 (class 1259 OID 18064)
+-- Name: venta; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.venta (
+    id_venta integer NOT NULL,
+    id_producto integer NOT NULL,
+    id_cliente integer NOT NULL,
+    id_vendedor integer NOT NULL,
+    fecha date NOT NULL,
+    monto integer NOT NULL
+);
+
+
+ALTER TABLE public.venta OWNER TO postgres;
+
+--
+-- TOC entry 3354 (class 0 OID 18043)
+-- Dependencies: 209
+-- Data for Name: cliente; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.cliente (id_cliente, id_comuna, cliente) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3355 (class 0 OID 18046)
+-- Dependencies: 210
+-- Data for Name: comuna; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.comuna (id_comuna, id_region, comuna) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3356 (class 0 OID 18049)
+-- Dependencies: 211
+-- Data for Name: galeria; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.galeria (id_galeria, id_comuna, galeria) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3357 (class 0 OID 18052)
+-- Dependencies: 212
+-- Data for Name: producto; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.producto (id_producto, id_tipo, producto, peso, id_galeria) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3358 (class 0 OID 18055)
+-- Dependencies: 213
+-- Data for Name: region; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.region (id_region, region) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3359 (class 0 OID 18058)
+-- Dependencies: 214
+-- Data for Name: tipo; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.tipo (id_tipo, tipo) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3360 (class 0 OID 18061)
+-- Dependencies: 215
+-- Data for Name: vendedor; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.vendedor (id_vendedor, id_comuna, id_galeria, vendedor, sexo, sueldo) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3361 (class 0 OID 18064)
+-- Dependencies: 216
+-- Data for Name: venta; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.venta (id_venta, id_producto, id_cliente, id_vendedor, fecha, monto) FROM stdin;
+\.
+
+
+--
+-- TOC entry 3192 (class 2606 OID 18068)
+-- Name: cliente cliente_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.cliente
+    ADD CONSTRAINT cliente_pkey PRIMARY KEY (id_cliente);
+
+
+--
+-- TOC entry 3194 (class 2606 OID 18070)
+-- Name: comuna comuna_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.comuna
+    ADD CONSTRAINT comuna_pkey PRIMARY KEY (id_comuna);
+
+
+--
+-- TOC entry 3196 (class 2606 OID 18072)
+-- Name: galeria galeria_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.galeria
+    ADD CONSTRAINT galeria_pkey PRIMARY KEY (id_galeria);
+
+
+--
+-- TOC entry 3198 (class 2606 OID 18074)
+-- Name: producto producto_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.producto
+    ADD CONSTRAINT producto_pkey PRIMARY KEY (id_producto);
+
+
+--
+-- TOC entry 3200 (class 2606 OID 18076)
+-- Name: region region_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.region
+    ADD CONSTRAINT region_pkey PRIMARY KEY (id_region);
+
+
+--
+-- TOC entry 3202 (class 2606 OID 18078)
+-- Name: tipo tipo_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.tipo
+    ADD CONSTRAINT tipo_pkey PRIMARY KEY (id_tipo);
+
+
+--
+-- TOC entry 3204 (class 2606 OID 18080)
+-- Name: vendedor vendedor_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.vendedor
+    ADD CONSTRAINT vendedor_pkey PRIMARY KEY (id_vendedor);
+
+
+--
+-- TOC entry 3205 (class 2606 OID 18081)
+-- Name: cliente fk_cliente_comuna; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.cliente
+    ADD CONSTRAINT fk_cliente_comuna FOREIGN KEY (id_comuna) REFERENCES public.comuna(id_comuna);
+
+
+--
+-- TOC entry 3206 (class 2606 OID 18086)
+-- Name: comuna fk_comuna_region; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.comuna
+    ADD CONSTRAINT fk_comuna_region FOREIGN KEY (id_region) REFERENCES public.region(id_region);
+
+
+--
+-- TOC entry 3207 (class 2606 OID 18091)
+-- Name: galeria fk_galeria_comuna; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.galeria
+    ADD CONSTRAINT fk_galeria_comuna FOREIGN KEY (id_comuna) REFERENCES public.comuna(id_comuna);
+
+
+--
+-- TOC entry 3208 (class 2606 OID 18096)
+-- Name: producto fk_producto_galeria; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.producto
+    ADD CONSTRAINT fk_producto_galeria FOREIGN KEY (id_galeria) REFERENCES public.galeria(id_galeria);
+
+
+--
+-- TOC entry 3209 (class 2606 OID 18101)
+-- Name: producto fk_producto_tipo; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.producto
+    ADD CONSTRAINT fk_producto_tipo FOREIGN KEY (id_tipo) REFERENCES public.tipo(id_tipo);
+
+
+--
+-- TOC entry 3210 (class 2606 OID 18106)
+-- Name: vendedor fk_vendedor_comuna; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.vendedor
+    ADD CONSTRAINT fk_vendedor_comuna FOREIGN KEY (id_comuna) REFERENCES public.comuna(id_comuna);
+
+
+--
+-- TOC entry 3211 (class 2606 OID 18111)
+-- Name: vendedor fk_vendedor_galeria; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.vendedor
+    ADD CONSTRAINT fk_vendedor_galeria FOREIGN KEY (id_galeria) REFERENCES public.galeria(id_galeria);
+
+
+--
+-- TOC entry 3212 (class 2606 OID 18116)
+-- Name: venta fk_venta_cliente; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.venta
+    ADD CONSTRAINT fk_venta_cliente FOREIGN KEY (id_cliente) REFERENCES public.cliente(id_cliente);
+
+
+--
+-- TOC entry 3213 (class 2606 OID 18121)
+-- Name: venta fk_venta_producto; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.venta
+    ADD CONSTRAINT fk_venta_producto FOREIGN KEY (id_producto) REFERENCES public.producto(id_producto);
+
+
+--
+-- TOC entry 3214 (class 2606 OID 18126)
+-- Name: venta fk_venta_vendedor; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.venta
+    ADD CONSTRAINT fk_venta_vendedor FOREIGN KEY (id_vendedor) REFERENCES public.vendedor(id_vendedor);
+
+
+-- Completed on 2022-09-04 22:23:17
+
+--
+-- PostgreSQL database dump complete
+--
+
---
+++ runStatements.sql
@@ -0,0 +1,52 @@
+--- Pregunta 2 ---
+SELECT pro.producto, ven.monto
+FROM public.venta as ven, public.producto as pro, public.tipo as tip
+WHERE tip.tipo='Pintura' AND  tip.id_tipo=pro.id_tipo AND pro.id_producto=ven.id_producto
+AND ven.fecha between '2021-01-01' and '2022-01-01'
+ORDER BY ven.monto ASC LIMIT 1;
+--- Pregunta 3 ---
+SELECT gal.galeria, vend.vendedor, COUNT(ven.id_vendedor) as cant
+FROM public.venta as ven, public.vendedor as vend, public.galeria as gal
+WHERE vend.id_galeria = gal.id_galeria AND ven.id_vendedor = vend.id_vendedor
+GROUP BY gal.galeria, vend.vendedor
+ORDER BY cant DESC LIMIT 1;
+--- Pregunta 4 ---
+SELECT DISTINCT ON (EXTRACT(year from ven.fecha)) (EXTRACT(year from ven.fecha)) as anio, cli.cliente, count(cli.cliente) as max
+from public.cliente as cli, public.venta as ven
+where cli.id_cliente = ven.id_cliente
+group by cli.cliente, anio ORDER BY anio DESC, max DESC;
+--- Pregunta 5 ---
+SELECT tip.tipo, count(ven.id_venta)
+FROM  public.venta as ven, public.producto as pro, public.tipo as tip
+WHERE tip.id_tipo = pro.id_tipo AND pro.id_producto = ven.id_producto
+GROUP BY tip.tipo;
+--- Pregunta 6 ---
+select reg.region, com.comuna, gal.galeria
+from public.region as reg, public.comuna as com, public.galeria as gal
+where reg.id_region = com.id_region and com.id_comuna = gal.id_comuna;
+--- Pregunta 7 ---
+select gal.galeria, to_char(ven.fecha,'Month'),max(ven.monto)
+from public.venta as ven, public.vendedor as vend, public.galeria as gal
+where ven.id_vendedor = vend.id_vendedor and
+vend.id_galeria = gal.id_galeria AND
+ven.fecha >= '2019-1-1' and ven.fecha < '2022-1-1'
+group by gal.galeria, to_char(ven.fecha,'Month') ORDER BY gal.galeria, to_date(to_char(ven.fecha,'Month'), 'Month') ASC;
+--- Pregunta 8 ---
+SELECT cliente, COUNT(fecha) FROM public.venta as ve, public.cliente as cl
+WHERE ve.id_cliente = cl.id_cliente 
+GROUP BY cliente, ve.fecha
+HAVING fecha>='2019-01-01' AND fecha<'2022-01-01'
+ORDER BY COUNT(fecha) DESC LIMIT 1;
+--- Pregunta 9 ---
+SELECT ga.galeria, COALESCE(h.hombres, 0) as hombres, COALESCE(h.promedio,0) as promedio_hombres, COALESCE(x.mujeres,0) as mujeres, COALESCE(x.promedio,0) as promedio_mujeres FROM galeria as ga, (SELECT galeria, COUNT(galeria) as hombres, AVG(ve.sueldo)::numeric(10,2) AS promedio FROM 
+public.vendedor AS ve, public.galeria AS ga WHERE
+ve.id_galeria = ga.id_galeria AND
+ve.sexo = 'H' GROUP BY galeria) as H FULL JOIN (SELECT galeria, COUNT(galeria) as mujeres, AVG(ve.sueldo)::numeric(10,2) AS promedio FROM 
+public.vendedor AS ve, public.galeria AS ga WHERE
+ve.id_galeria = ga.id_galeria AND
+ve.sexo = 'M' GROUP BY galeria) as X ON H.galeria = X.galeria WHERE ga.galeria = X.galeria or ga.galeria = H.galeria;
+--- Pregunta 10 ---
+SELECT to_char(ve.fecha,'Month') as mes, MAX(pr.peso) FROM public.venta as ve, public.producto as pr 
+WHERE pr.id_producto = ve.id_producto 
+AND ve.fecha < '2022-01-01'
+AND ve.fecha >= '2021-01-01' GROUP by mes ORDER BY to_date(to_char(ve.fecha,'Month'), 'Month') DESC;
